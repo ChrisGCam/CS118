d21589729e35a7924d7eeb72d9fdf98de1104632
ChrisGCam <christian.cam@ucla.edu>
2017-03-09 11:29:09 -0800

    Socket Creation/Binding

--- Project 2/server.c
+++ Project 2/server.c
@@ -3,18 +3,72 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+// For UDP socket programming, the following tutorial was used: https://www.cs.rutgers.edu/~pxk/417/notes/sockets/udp.html
 
 int const MAX_PACKET_SIZE = 1024;
-int const HEADER_SIZE = 24;
+int HEADER_SIZE;
+int MAX_PAYLOAD_SIZE;
 
 int main(int argc, char *argv[])
 {
-	fprintf(stdout, "%i \n", sizeof(socklen_t));
+	// Check for correct argument length
+	if (argc != 2) {
+		fprintf(stderr, "ERROR: incorrect arguments.\n");
+		exit(1);
+	}
 	
+	HEADER_SIZE = sizeof(int) * 6;
+	MAX_PAYLOAD_SIZE = MAX_PACKET_SIZE - HEADER_SIZE;
 	
+	struct sockaddr_in myAddr, clientAddr;
+	socklen_t myAddrLen = sizeof(myAddr);
+	socklen_t clientAddrLen = sizeof(clientAddr);
+	int fd, portno, recvLen;
+	char buf[MAX_PACKET_SIZE];
+	
+	int len, seqNum, wnd, ret, syn, fin;
+	
+	// Get the portnumber
+	portno = atoi(argv[1]);
+	if(portno < 0)
+	{
+		fprintf(stderr, "ERROR: invalid port number.\n");
+		exit(1);
+	}
+	
+	fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (fd < 0) {
+		fprintf(stderr, "ERROR: socket creation failed.\n");
+		exit(1);
+	}
+	
+	bzero((char *)&myAddr, sizeof(myAddr));
+	myAddr.sin_family = AF_INET;
+	myAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	myAddr.sin_port = htons(portno);
+
+	if (bind(fd, (struct sockaddr *)&myAddr, sizeof(myAddr)) < 0) {
+		perror("bind failed");
+		return 0;
+	}
+
+	if (getsockname(fd, (struct sockaddr *)&myAddr, &myAddrLen) < 0) {
+		perror("getsockname failed");
+		return 0;
+	}
+	
+	// Get a packet from the client
+	getPacket(fd, buf, &len, (struct sockaddr *)&clientAddr, &clientAddrLen, &seqNum, &wnd, &ret, &syn, &fin);
+	printf("Received packet from %s:%d\nData: %s\n\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port), buf);
+	
+	close(fd);
 	return 0;
 }
 
@@ -26,6 +80,7 @@ int sendPacket(int sockfd, char* message, size_t len, const struct sockaddr *des
 	
 	int packetLen = HEADER_SIZE + len;
 	
+	// Payload too large
 	if(packetLen > MAX_PACKET_SIZE)
 		return -1;
 	
@@ -34,6 +89,7 @@ int sendPacket(int sockfd, char* message, size_t len, const struct sockaddr *des
 	
 	int intSize = sizeof(int);
 	
+	// Copy in the header information
 	memcpy(toSend, &len, intSize);
 	memcpy(toSend + intSize, &seqNum, intSize);
 	memcpy(toSend + intSize*2, &wnd, intSize);
@@ -42,23 +98,30 @@ int sendPacket(int sockfd, char* message, size_t len, const struct sockaddr *des
 	memcpy(toSend + intSize*5, &fin, intSize);
 	
 	result = sendto(sockfd, toSend, packetLen, 0, dest_addr, dest_len);
+	if(result < 0)
+	{
+		perror("sendto failed.");
+		return -1;
+	}
 	
 	free(toSend);
 	
 	return result;
 }
 
+// Wrapper function for recvfrom that also gets the header contents from the packet
+// Also gets the sock address of the sender
 int getPacket(int sockfd, void* message, size_t* len, struct sockaddr *src_addr, socklen_t * src_len, int* seqNum, int* wnd, int* ret, int* syn, int* fin)
 {
 	int result = -1;
-	int packetLen = HEADER_SIZE + MAX_PACKET_SIZE;
+	int packetLen = MAX_PACKET_SIZE;
 	
 	char* received = malloc(packetLen);
 	bzero(received, packetLen);
 	
 	result = recvfrom(sockfd, received, packetLen, 0, src_addr, src_len);
 	
-	memcpy(message, received + HEADER_SIZE, MAX_PACKET_SIZE - HEADER_SIZE);
+	memcpy(message, received + HEADER_SIZE, MAX_PAYLOAD_SIZE);
 	
 	int intSize = sizeof(int);
 	
